d9fde0d6982be1412a741a9c8e3f1671c229d6e2
gabytrejo <gabytrejo303@gmail.com>
2017-06-21 16:41:43 +0000

    Responsive Nav with translations

--- js/texts.js
+++ js/texts.js
@@ -3,14 +3,24 @@ var translations = {
     translation: {
       "nav-skills": "Habilidades",
       "nav-portfolio": "Portafolio",
-      "nav-contact": "Contacto"
+      "nav-contact": "Contacto",
+      "title-ing": "Ingeniera Mecatrónica",
+      "title-vle": "Desarrolladora AVOA",
+      "content-ing": "Para mi la ingeniería es un medio para ayudar y resolver problemas.",
+      "content-coder": "Programadora Front End Junior enfocada en funcionalidad, claridad y buen diseño.",
+      "content-vle": "Ambientes Virtuales y Objetos de Aprendizaje enfocados a la educación continua y cursos en línea."
     }
   },
   en: {
     translation: {
       "nav-skills": "Skills",
       "nav-portfolio": "Portfolio",
-      "nav-contact": "Contact"
+      "nav-contact": "Contact",
+      "title-ing": "Mechatronics Engineer",
+      "title-vle": "VLE Developer",
+      "content-ing": "For me engineering is a means to help and solve problems.",
+      "content-coder": "Junior Front-end Developer who focuses on functionality, clean writing and good design.",
+      "content-vle": "Virtual Learning Environments  and Learning Objects developer (e-learning)."
     }
   }
 };
@@ -22,11 +32,43 @@ var texts = [
     key: "nav-skills"
   },
   {
+    id: "nav-skills-mobile",
+    key: "nav-skills"
+  },
+  {
     id: "nav-portfolio",
+    key: "nav-portfolio"
+  },
+  {
+    id: "nav-portfolio-mobile",
     key: "nav-portfolio"
   },
   {
     id: "nav-contact",
     key: "nav-contact"
+  },
+  {
+    id: "nav-contact-mobile",
+    key: "nav-contact"
+  },
+  {
+    id: "title-ing",
+    key: "title-ing"
+  },
+  {
+    id: "title-vle",
+    key: "title-vle"
+  },
+  {
+    id: "content-ing",
+    key: "content-ing"
+  },
+  {
+    id: "content-coder",
+    key: "content-coder"
+  },
+  {
+    id: "content-vle",
+    key: "content-vle"
   }
 ];

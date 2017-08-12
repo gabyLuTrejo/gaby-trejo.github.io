9dcc7690592412aa28af32f0bcedf2ac2ca52980
Gaby Trejo <gabytrejo303@gmail.com>
2017-08-12 16:15:20 -0500

    Projects added & styled

--- js/texts.js
+++ js/texts.js
@@ -13,10 +13,14 @@ var translations = {
       "title-about": "¡Hola, Soy Gaby!",
       "content-about": "Soy amante de los deportes principalmente los acuáticos, aficionada de la lectura e interesada en las historias de esos lugares que no siempre aparecen en el mapa.",
       "title-moodle": "AVA: IntroRobótica",
+      "info-yape": "Aplicación móvil que permite el registro de usuarios (celular y tarjeta de crédito) por medio de una api.",
+      "info-pokedex": "Página de pokemones que brinda informción sobre ellos.",
+      "info-passeante": "Proyecto móvil colaborativo ganador del primer Hackaton Laboratoria.",
       "info-free-love": "Replica 'pixel perfect' de sitio.",
-      "info-passeante": "Proyecto movil colaborativo ganador del primer Hackaton Laboratoria.",
-      "info-daventon": "Aplicación móvil inspirada en lyft.",
       "info-cuptronic": "Proyecto escolar de un brazo robotico que decora cupcakes.",
+      "info-moodle": "Curso introductorio de robótica en línea que estoy creando.",
+      "info-daventon": "Aplicación móvil inspirada en lyft.",
+      "info-cdmx": "Página web creada colaborativamente para dar a conocer un poco de la CDMX a niños de Wichita Kansas.",
       "title-contact": "¡No olvides ponerte en contacto!",
       "phrase": "¡Haz que suceda!"
     }
@@ -35,10 +39,14 @@ var translations = {
       "title-about": "Hi, I am Gaby!",
       "content-about": "I am a lover of sports, mainly aquatic, I am fond of reading and interested in the stories of those places that do not always appear on the map.",
       "title-moodle": "VLE: IntroRobotics",
-      "info-free-love": "Pixel perfect site replication.",
+      "info-yape": "Mobile application that allows users registration (cellphone and credit card) through an api.",
+      "info-pokedex": "Pokemon's page that provides information about them.",
       "info-passeante": "Wining mobile project from the first Laboratoria's Hackathon.",
-      "info-daventon": "Mobile app, inspired by lyft app.",
+      "info-free-love": "Pixel perfect site replication.",
       "info-cuptronic": "School's project of a robotic arm that decorates cupcakes.",
+      "info-moodle": "Introductory Robotics online course I'm creating.",
+      "info-daventon": "Mobile app, inspired by lyft app.",
+      "info-cdmx": "Website created collaboratively to make known Mexico city to Wichita Kansas' children.",
       "title-contact": "Don't forget to keep in touch!",
       "phrase": "Make it happen!"
     }
@@ -119,6 +127,18 @@ var texts = [
     id: "title2-moodle",
     key: "title-moodle"
   },
+    {
+    id: "info-yape",
+    key: "info-yape"
+  },
+    {
+    id: "info-pokedex",
+    key: "info-pokedex"
+  },
+    {
+    id: "info-passeante",
+    key: "info-passeante"
+  },
   {
     id: "info-freelancer",
     key: "info-free-love"
@@ -128,16 +148,20 @@ var texts = [
     key: "info-free-love"
   },
     {
-    id: "info-passeante",
-    key: "info-passeante"
-  },
-    {
     id: "info-daventon",
     key: "info-daventon"
   },
     {
     id: "info-cuptronic",
     key: "info-cuptronic"
+  },
+    {
+    id: "info-moodle",
+    key: "info-moodle"
+  },
+    {
+    id: "info-cdmx",
+    key: "info-cdmx"
   },
   {
     id: "title-contact",
